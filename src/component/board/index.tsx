5480075640f6961bafd0a0d9b70e6f30dc9e3fcb
ch.hashemi <ch.seyedhashemi@gmail.com>
2021-06-28 00:05:30 +0430

    add cards to board cmp

--- src/component/board/index.tsx
+++ src/component/board/index.tsx
@@ -1,3 +1,27 @@
-export const Board = () => {
-  return <div>board</div>;
+import { useEffect } from "react";
+import { connect, useSelector } from "react-redux";
+import { IUser } from "../../model/user";
+import { IReduxState } from "../../redux/appState";
+import { UserCard } from "./UserCard";
+import style from "./userCard.module.scss";
+
+interface IProps {
+  users: Array<IUser>;
+}
+
+const BoardComponent = (props: IProps) => {
+  return (
+    <div className={style.board}>
+      {props.users.map((item, index) => {
+        return <UserCard user={item} key={index} />;
+      })}
+    </div>
+  );
 };
+const mapStateToProps = (state: IReduxState) => {
+  return {
+    users: state.users,
+  };
+};
+
+export const Board = connect(mapStateToProps, {})(BoardComponent);
