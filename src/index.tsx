7ee8b59b1fa34f4a1db5e1faf4dc5f52e47023a6
ch.hashemi <ch.seyedhashemi@gmail.com>
2021-06-27 21:26:31 +0430

    add redux provider and persist to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./component/app/App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, Store2 } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={Store2}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
